1af4655ee5311042f73f3a644954091c254bad65
ZavaliEbalo <razrazrazetohardbaz@gmail.com>
2021-10-27 00:31:55 +0300

    Updates

---
+++ precache-manifest.1b3cf0c107813cbb6f84ca7bfb3522e7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc15ec6c29537d6277befbb84bbc4b02",
+    "url": "/currency/index.html"
+  },
+  {
+    "revision": "a0cd8ba104380005caee",
+    "url": "/currency/static/css/2.2cfa1b23.chunk.css"
+  },
+  {
+    "revision": "ade5f5e88b55b2eb1a17",
+    "url": "/currency/static/css/main.48330483.chunk.css"
+  },
+  {
+    "revision": "a0cd8ba104380005caee",
+    "url": "/currency/static/js/2.f6e81232.chunk.js"
+  },
+  {
+    "revision": "6e1720d6b85775e56d8dc136b868cc77",
+    "url": "/currency/static/js/2.f6e81232.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ade5f5e88b55b2eb1a17",
+    "url": "/currency/static/js/main.2fbad78f.chunk.js"
+  },
+  {
+    "revision": "52fa3b2951c2fae3bb02",
+    "url": "/currency/static/js/runtime-main.1c7c7791.js"
+  },
+  {
+    "revision": "4c51214613dc35465553e71e131a4cb3",
+    "url": "/currency/static/media/logo.4c512146.svg"
+  }
+]);
